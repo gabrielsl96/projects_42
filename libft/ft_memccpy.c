e00b6d20ef9ba1b2d433c720897152e3ab792951
Gabriel Sousa Lucas <gsousalucas@outlook.com>
2021-02-12 15:03:59 -0300

    correcao ft_memccpy.c

--- libft/ft_memccpy.c
+++ libft/ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: gsousa-l <gsousa-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/03 21:59:06 by gsousa-l          #+#    #+#             */
-/*   Updated: 2021/02/09 18:24:09 by gsousa-l         ###   ########.fr       */
+/*   Updated: 2021/02/12 15:03:40 by gsousa-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,19 @@
 
 void    *ft_memccpy(void *dest, const void *src, int c, size_t n)
 {
-    size_t count_src;
-    size_t count_dest;
+    size_t count;
+    unsigned char *ptr_dest;
+    unsigned char *ptr_src;
     
-    count_src = 0;
-    count_dest = 0;
-    while (count_src < n &&*((char *) src + count_src) != '\0')
+    ptr_dest = (unsigned char *)dest;
+    ptr_src = (unsigned char *)src;
+    count = 0;
+    while (count < n)
     {
-        if (*((char *) src + count_src) != c)
-        {
-            *((char *) dest + count_dest) = *((char *) src + count_src);
-            count_dest++;
-        }
-        else
-        {
-            *((char *) dest + count_dest) = *((char *) src + count_src);
-            return dest;
-        }
-        count_src++;
+        ptr_dest[count] = ptr_src[count];
+        if (ptr_src[count] == (unsigned char)c)
+            return ((void *)ptr_src + i + 1);
+        count++;
     }
-    return NULL;
+    return (NULL);
 }
