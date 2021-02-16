8f65a48f170918643604a339c06595971edf8498
Gabriel Sousa Lucas <gsousalucas@outlook.com>
2021-02-16 20:33:32 -0300

    correcao ft_strdup

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: gsousa-l <gsousa-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 18:14:32 by gsousa-l          #+#    #+#             */
-/*   Updated: 2021/02/16 20:14:10 by gsousa-l         ###   ########.fr       */
+/*   Updated: 2021/02/16 20:33:15 by gsousa-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ char	*ft_strdup(const char *s)
 	count = 0;
 	len = ft_strlen(s);
 	dest = malloc(len + 1);
+	if (dest == NULL)
+		return (NULL);
 	while (count < len)
 	{
 		dest[count] = s[count];
