b9793c11e7412f2e06c90fcb3c24c562e2dc6b7c
Gabriel Sousa lucas <gsousalucas@outlook.com>
2021-02-16 18:09:24 -0300

    correcao ft_calloc.c

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: gsousa-l <gsousa-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 17:20:52 by gsousa-l          #+#    #+#             */
-/*   Updated: 2021/02/16 17:20:52 by gsousa-l         ###   ########.fr       */
+/*   Updated: 2021/02/16 18:08:14 by gsousa-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,11 @@
 void    *ft_calloc(size_t nmemb, size_t size)
 {
     void *ptr;
-    int count;
-
-    count = 0;
-    if (nmemb == 0 || size == 0)
-        return (NULL);
-    ptr = malloc(nmemb * size);
-    while(count < nmemb)
-    {
-        ptr[count] = 0;
-        count++
-    }
+	
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	ft_bzero(ptr, nmemb);
     return (ptr);
 }
+
