cbb812cee32f0d84d7a9c04d374f1ae0253292bb
Gabriel Sousa Lucas <gsousalucas@outlook.com>
2021-02-19 18:42:43 -0300

    correcao ft_strim

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: gsousa-l <gsousa-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/17 14:32:34 by gsousa-l          #+#    #+#             */
-/*   Updated: 2021/02/18 11:03:58 by gsousa-l         ###   ########.fr       */
+/*   Updated: 2021/02/19 18:39:03 by gsousa-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,27 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	start;
-	size_t	end;
-	char	*new;
-
+	size_t    len;
+    size_t    end;
+    char    *new;
+    char    *str_temp;
+	
 	if (!s1 || !set)
-		return (NULL);
-	start = 0;
-	while (s1[start] && ft_strchr(set, s1[start]))
-		start++;
-	end = ft_strlen(s1 + start);
-	while (end - 1 && ft_strchr(set, s1[end - 1]))
-		end--;
-	if (!(new = malloc(sizeof(char *) * (end + 1))))
-		return (NULL);
-	while (start < end)
-	{
-		new[start] = s1[start];
-		start++;
-	}
-	new[end] = '\0';
+        return (NULL);
+    str_temp = ft_strnstr(s1, set, strlen(s1));
+    if (!str_temp)
+      return ((char *)s1);
+    len = strlen(str_temp) - 2;
+    end = len;
+    while (end - 1 && !ft_strnstr(str_temp + end, set, len))
+        end--;
+    if (len != end)
+        len = end;
+    if (!(new = malloc(sizeof(char) * (len))))
+        return (NULL);
+    str_temp++;
+    while (--len)
+        *new++ = *str_temp++;
+    *new = '\0';
 	return (new);
 }
