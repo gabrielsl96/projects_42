f6e009b4dac5e2c49577dd168efd692a2bd7a6c1
Gabriel Sousa Lucas <gsousalucas@outlook.com>
2021-02-19 22:20:45 -0300

    correc. ft_strnstr

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: gsousa-l <gsousa-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/11 10:50:24 by gsousa-l          #+#    #+#             */
-/*   Updated: 2021/02/19 22:12:37 by gsousa-l         ###   ########.fr       */
+/*   Updated: 2021/02/19 22:20:20 by gsousa-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,23 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t count;
-	size_t count_need;
-	size_t len_need;
-
-	count = 0;
-	len_need = ft_strlen(needle);
-	if (len_need == 0 || len == 0)
-		return ((char *)haystack);
-	while (haystack[count] && count < len)
-	{
-		count_need = -1;
-		while ((needle[count_need + 1])
-				&& (needle[count_need + 1] == haystack[count_need + count + 1]))
-			count_need++;
-		
-		if (count_need == len_need)
-			return ((char *)haystack + count);
-		count++;
-	}
+	size_t i;
+    size_t j;
+    
+    
+	if (!needle || *needle == '\0')
+        return ((char *) haystack);
+    i = 0;
+    while (haystack[i] && i < len)
+    {
+        j = 0;
+        while (needle[j] == haystack[j + i] && i + j < len)
+        {
+            if (needle[j + 1] == '\0')
+                return ((char *)haystack + i);
+            j++;
+        }
+        i++;
+    }
 	return (NULL);
 }
