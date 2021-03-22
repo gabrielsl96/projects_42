f5b33c125011b9dd686ce8589cfe539a0a99c532
Gabriel Sousa Lucas <gsousalucas@outlook.com>
2021-03-22 10:38:54 -0300

    fix bug - '0' => ' '

--- ft_printf/src/ft_fill_zero.c
+++ ft_printf/src/ft_fill_zero.c
@@ -6,7 +6,7 @@
 /*   By: gsousa-l <gsousa-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/21 15:39:30 by gsousa-l          #+#    #+#             */
-/*   Updated: 2021/03/22 10:23:08 by gsousa-l         ###   ########.fr       */
+/*   Updated: 2021/03/22 10:37:43 by gsousa-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ char	*ft_fill_zero(char *str, int size)
 
 	len = ft_strlen(str);
 	zeros = 0;
+	if (ft_strlen(str) == 1 && str[0] == '0')
+		str[0] = ' ';
 	if(len < size)
 		zeros +=  size - len;
 	aux = malloc(sizeof(aux) * (len + zeros + 1));
