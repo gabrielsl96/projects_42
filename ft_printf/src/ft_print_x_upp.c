86a977bc66d39a67773aa190fbba6c6e75496587
Gabriel Sousa Lucas <gsousalucas@outlook.com>
2021-03-25 20:42:46 -0300

    norme

--- ft_printf/src/ft_print_x_upp.c
+++ ft_printf/src/ft_print_x_upp.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gsousa-l <gsousa-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/19 11:15:39 by gsousa-l          #+#    #+#             */
-/*   Updated: 2021/03/25 20:05:47 by gsousa-l         ###   ########.fr       */
+/*   Created: 2021/03/25 20:28:16 by gsousa-l          #+#    #+#             */
+/*   Updated: 2021/03/25 20:42:23 by gsousa-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,53 +27,14 @@ void	ft_upperstr(char *str)
 	free(aux);
 }
 
-void	print_width_x_upp(char *str, t_params *parameters, int len, int *size)
+char	*set_string_x_upp(long num, t_params *parameters)
 {
-	int signal;
-	int i;
-
-	signal = 0;
-	i = parameters->value_width;
-	while (i - len > 0)
-	{
-		if (parameters->zero && parameters->value_precision < 0 && len > 0)
-		{
-			if (str[0] == '-' && signal == 0)
-			{
-				ft_print_char('-', size);
-				signal = 1;
-				continue ;
-			}
-			ft_print_char('0', size);
-		}
-		else
-			ft_print_char(' ', size);
-		i--;
-	}
-	ft_print_str(str + signal, size);
-}
-
-void	print_minus_x_upp(char *str, t_params *parameters, int len, int *size)
-{
-	int i;
-
-	i = parameters->value_width;
-	ft_print_str(str, size);
-	while (i - len > 0)
-	{
-		ft_print_char(' ', size);
-		i--;
-	}
-}
-
-char	*set_string_x_upp(int num, t_params *parameters)
-{
-	int		i;
 	char	*str;
 	char	*aux;
+	int		i;
 
 	str = ft_dec_to_base(num, 8, 16);
-	ft_upperstr(aux);
+	ft_upperstr(str);
 	if (*str == '0' && !parameters->value_precision && parameters->precision)
 		str[0] = '\0';
 	if (parameters->zero && parameters->precision == false
@@ -93,13 +54,56 @@ char	*set_string_x_upp(int num, t_params *parameters)
 	return (aux);
 }
 
+void	print_minus_x_upp(char *str, t_params *parameters, int len, int *size)
+{
+	int i;
+
+	i = parameters->value_width;
+	ft_print_str(str, size);
+	while (i - len > 0)
+	{
+		ft_print_char(' ', size);
+		i--;
+	}
+}
+
+void	print_width_x_upp(char *str, t_params *parameters, int len, int *size)
+{
+	int i;
+	int signal;
+
+	signal = 0;
+	i = parameters->value_width;
+	while (i - len > 0)
+	{
+		if (parameters->zero && parameters->value_precision < 0
+			&& len > 0)
+		{
+			if (str[0] == '-' && signal == 0)
+			{
+				ft_print_char('-', size);
+				signal == 1;
+				continue ;
+			}
+			ft_print_char('0', size);
+		}
+		else
+			ft_print_char(' ', size);
+		i--;
+	}
+	ft_print_str(str, size);
+}
+
 void	ft_print_x_upp(va_list args, t_params *parameters, int *size)
 {
 	char	*str;
 	long	num;
+	int		i;
 	int		len;
 
-	str = set_string_x_upp(va_arg(args, int), parameters);
+	len = 0;
+	num = va_arg(args, int);
+	str = set_string_x_upp(num, parameters);
 	len = ft_strlen(str);
 	if (parameters->minus == true)
 		print_minus_x_upp(str, parameters, len, size);
