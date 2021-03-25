8f97dd2b4f22498f362d92e1cf058213bf67ade4
Gabriel Sousa Lucas <gsousalucas@outlook.com>
2021-03-25 17:08:39 -0300

    norme

--- ft_printf/src/ft_print_str.c
+++ ft_printf/src/ft_print_str.c
@@ -6,7 +6,7 @@
 /*   By: gsousa-l <gsousa-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/25 16:44:08 by gsousa-l          #+#    #+#             */
-/*   Updated: 2021/03/25 16:46:24 by gsousa-l         ###   ########.fr       */
+/*   Updated: 2021/03/25 17:08:32 by gsousa-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,5 @@ void	ft_print_str(char *str, int *size)
 
 	i = 0;
 	while(str[0])
-			ft_print_char(str[i], size);
+			ft_print_char(str[i++], size);
 }
