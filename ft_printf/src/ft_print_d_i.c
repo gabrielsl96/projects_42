c07cea66acbab25a316ae22c1dc5e1cbe9011ec5
Gabriel Sousa lucas <gsousa-l@c1r1p4.42sp.org.br>
2021-03-25 09:14:34 -0300

    leak memory

--- ft_printf/src/ft_print_d_i.c
+++ ft_printf/src/ft_print_d_i.c
@@ -6,7 +6,7 @@
 /*   By: gsousa-l <gsousa-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/19 09:41:23 by gsousa-l          #+#    #+#             */
-/*   Updated: 2021/03/24 13:11:02 by gsousa-l         ###   ########.fr       */
+/*   Updated: 2021/03/25 00:04:38 by gsousa-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -98,10 +98,11 @@ void	ft_print_d_i(va_list args, t_params *parameters, int *size)
 		}
 		while(*aux != '\0')
 			ft_print_char(*aux++, size);
+		free(aux - len);
 		return ;
 	}
 	else
 		ft_putstr_fd(aux, 1);
-	*size += ft_strlen(&(aux[0]));
-	free(&(aux[0]));
+	*size += ft_strlen(aux);
+	free(aux);
 }
