884d72c644679f38c55a909f6d11243b3b885203
Gabriel Sousa Lucas <gsousalucas@outlook.com>
2021-03-25 15:06:55 -0300

    correction zero

--- ft_printf/src/ft_print_s.c
+++ ft_printf/src/ft_print_s.c
@@ -6,7 +6,7 @@
 /*   By: gsousa-l <gsousa-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 18:56:42 by gsousa-l          #+#    #+#             */
-/*   Updated: 2021/03/24 13:14:26 by gsousa-l         ###   ########.fr       */
+/*   Updated: 2021/03/25 15:06:43 by gsousa-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,12 +57,26 @@ void	ft_print_s(va_list args, t_params *parameters, int *size)
 	else if(parameters->width == true)
 	{
 		i = parameters->value_width;
-		while((i - str_len) > 0)
+		while(i - len > 0)
 		{
-			ft_print_char(' ', size);
+			if (parameters->zero && parameters->value_precision < 0
+			&& len > 0)
+			{
+				if (*aux == '-')
+				{
+					ft_print_char(*aux++, size);
+					continue ;
+				}
+				ft_print_char('0', size);
+			}
+			else
+				ft_print_char(' ', size);
 			i--;
 		}
-		ft_putstr_fd(aux, 1);
+		while(*aux != '\0')
+			ft_print_char(*aux++, size);
+		free(aux - len);
+		return ;
 	}
 	else
 		ft_putstr_fd(aux, 1);
