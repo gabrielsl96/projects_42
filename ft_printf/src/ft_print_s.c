8d7fb2a7e882c4bbf78627dc18d7cf3e462358d4
Gabriel Sousa Lucas <gsousalucas@outlook.com>
2021-03-25 15:38:36 -0300

    fix bugs - fill zero

--- ft_printf/src/ft_print_s.c
+++ ft_printf/src/ft_print_s.c
@@ -6,7 +6,7 @@
 /*   By: gsousa-l <gsousa-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 18:56:42 by gsousa-l          #+#    #+#             */
-/*   Updated: 2021/03/25 15:08:47 by gsousa-l         ###   ########.fr       */
+/*   Updated: 2021/03/25 15:38:06 by gsousa-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,8 +59,7 @@ void	ft_print_s(va_list args, t_params *parameters, int *size)
 		i = parameters->value_width;
 		while(i - str_len > 0)
 		{
-			if (parameters->zero && parameters->value_precision < 0
-			&& str_len > 0)
+			if (parameters->zero && parameters->precision == false)
 			{
 				if (*aux == '-')
 				{
