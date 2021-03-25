50a5e93d83d71d575cd66a40491b36d18fcd38fb
Gabriel Sousa Lucas <gsousalucas@outlook.com>
2021-03-25 15:15:17 -0300

    fix bugs

--- ft_printf/src/ft_print_p.c
+++ ft_printf/src/ft_print_p.c
@@ -6,7 +6,7 @@
 /*   By: gsousa-l <gsousa-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/19 16:53:13 by gsousa-l          #+#    #+#             */
-/*   Updated: 2021/03/25 15:13:47 by gsousa-l         ###   ########.fr       */
+/*   Updated: 2021/03/25 15:15:11 by gsousa-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,8 +41,8 @@ void	ft_print_p(va_list args, t_params *parameters, int *size)
 	len = 0;
 	num = va_arg(args, long long);
 	str = ft_dec_to_base_p(num, 16, 16);
-	if ((unsigned int)num == 0 && !parameters->value_precision
-		&& parameters->precision)
+	if (((unsigned int)num == 0 || num = NULL) &&
+	!parameters->value_precision && parameters->precision)
 			str[0] = '\0';
 	else if(parameters->precision == true)
 	{
