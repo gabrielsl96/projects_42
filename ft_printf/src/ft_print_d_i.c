0bf8bd3abc5e8e6459337c0d2d915e5281871ba2
Gabriel Sousa Lucas <gsousalucas@outlook.com>
2021-03-25 17:26:24 -0300

    norme

--- ft_printf/src/ft_print_d_i.c
+++ ft_printf/src/ft_print_d_i.c
@@ -6,7 +6,7 @@
 /*   By: gsousa-l <gsousa-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/19 09:41:23 by gsousa-l          #+#    #+#             */
-/*   Updated: 2021/03/25 17:00:30 by gsousa-l         ###   ########.fr       */
+/*   Updated: 2021/03/25 17:25:58 by gsousa-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	print_width_d_i(char *str, t_params *parameters, int len, int *size)
 
 	signal = 0;
 	i = parameters->value_width;
-	while(i - len > 0)
+	while (i - len > 0)
 	{
 		if (parameters->zero && parameters->value_precision < 0 && len > 0)
 		{
@@ -45,7 +45,7 @@ void	print_minus_d_i(char *str, t_params *parameters, int len, int *size)
 	i = 0;
 	ft_print_str(str, size);
 	i = parameters->value_width;
-	while(i - len > 0)
+	while (i - len > 0)
 	{
 		ft_print_char(' ', size);
 		i--;
@@ -66,7 +66,7 @@ char	*fill_zero_minus(char *str, int size, char type)
 	len = ft_strlen(str);
 	if (len <= size)
 		if (type == '0')
-			zeros +=  size - len;
+			zeros += size - len;
 		else
 			zeros += size - len + signal;
 	aux = malloc(sizeof(aux) + (len + zeros + 1));
@@ -79,48 +79,44 @@ char	*fill_zero_minus(char *str, int size, char type)
 
 char	*set_string_d_i(int num, t_params *parameters)
 {
-	char *str;
-	int i;
+	char	*str;
+	char	*aux;
+	int		i;
 
 	i = 0;
 	str = ft_itoa(num);
-	if (*str == '0' && !parameters->value_precision
-		&& parameters->precision)
-			str[0] = '\0';
+	if (*str == '0' && !parameters->value_precision && parameters->precision)
+		str[0] = '\0';
 	if (parameters->zero && parameters->precision == false
 	&& parameters->minus == false)
 	{
 		i = parameters->value_width;
-		return (fill_zero_minus(str, i, '0'));
+		aux = fill_zero_minus(str, i, '0');
 	}
 	else if (parameters->precision == true)
 	{
 		i = parameters->value_precision;
-		return (fill_zero_minus(str, i, '-'));
+		aux = fill_zero_minus(str, i, '-');
 	}
 	else
-		return (ft_strdup(str));
+		aux = ft_strdup(str);
 	free(str);
 }
 
 void	ft_print_d_i(va_list args, t_params *parameters, int *size)
 {
-	int num;
-	int i;
-	int len;
-	char *str;
+	int		num;
+	int		i;
+	int		len;
+	char	*str;
 
 	len = 0;
 	str = set_string_d_i(va_arg(args, int), parameters);
 	len = ft_strlen(str);
 	if (parameters->minus == true)
-	{
 		print_minus_d_i(str, parameters, len, size);
-	}
 	else if (parameters->width == true)
-	{
 		print_width_d_i(str, parameters, len, size);
-	}
 	else
 		ft_print_str(str, size);
 	free(str);
