ed8f1bca0ca63c0e7eabf28da9834a8275b19181
Gabriel Sousa Lucas <gsousalucas@outlook.com>
2021-03-25 15:08:13 -0300

    correction syntaxe

--- ft_printf/src/ft_print_s.c
+++ ft_printf/src/ft_print_s.c
@@ -6,7 +6,7 @@
 /*   By: gsousa-l <gsousa-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 18:56:42 by gsousa-l          #+#    #+#             */
-/*   Updated: 2021/03/25 15:06:43 by gsousa-l         ###   ########.fr       */
+/*   Updated: 2021/03/25 15:07:51 by gsousa-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,10 +57,10 @@ void	ft_print_s(va_list args, t_params *parameters, int *size)
 	else if(parameters->width == true)
 	{
 		i = parameters->value_width;
-		while(i - len > 0)
+		while(i - str_len > 0)
 		{
 			if (parameters->zero && parameters->value_precision < 0
-			&& len > 0)
+			&& str_len > 0)
 			{
 				if (*aux == '-')
 				{
